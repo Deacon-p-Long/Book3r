b0fb276fd0c3cfed19439b51b81e473e611397d2
Deacon Long <djpl.0316@icloud.com>
2025-04-08 17:43:19 -0500

    Changed base.html to show a simple welcome message. The index view no longer passes any variables to base.html Created a new template called search_results.html which, for now, only displays a value passed into it via the URL. Created the book_search view to run the search_results.html template. it functions similarly to how the index view used to.

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def index (request):
-    name = request.GET.get ("name") or "world"
-    return render (request, "base.html", {"name": name})
+    return render (request, "base.html")
+
+def book_search (request):
+    search_query = request.GET.get ("q")
+    return render (request, "search_results.html", {"search_query": search_query})
