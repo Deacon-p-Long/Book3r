07f09975a4c6eeef677c165d56b89c4cb9ac0ef8
Deacon Long <djpl.0316@icloud.com>
2025-04-08 16:51:45 -0500

    Modified the base.html template to accept a name variable as described in exercise 1.07. Unlike the exercise in the book, I left in the GET request to allow a user to input a value through the URL.

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 def index (request):
-    #name = request.GET.get ("name") or "world"
-    #return HttpResponse ("Hello {}!" .format (name))
-    return render (request, "base.html")
+    name = request.GET.get ("name") or "world"
+    return render (request, "base.html", {"name": name})
